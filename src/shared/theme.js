9b7b46b1ce44982a7a1b83888a31159732c86970
Konstantin M <humbl3dev@gmail.com>
2020-03-23 22:02:50 -0700

    extract site theme into module

---
+++ src/shared/theme.js
@@ -0,0 +1,10 @@
+export const siteTheme = {
+  colors: {
+    black: "#070918",
+    white: "#fff",
+    lightGray: "#F3F5FA",
+    red: "#B60045",
+    blue: "#0051FF",
+    textColor: "rgba(7, 9, 24, .75)",
+  },
+}
