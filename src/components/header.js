d5ed5914d0c40d8fe22abd312f7d86d282f5520c
Konstantin M <humbl3dev@gmail.com>
2020-03-23 22:14:51 -0700

    working on header

--- src/components/header.js
+++ src/components/header.js
@@ -5,27 +5,7 @@ import { css } from "@emotion/core"
 import styled from "@emotion/styled"
 import { useTheme } from "emotion-theming"
 
-const TopHeader = styled.header`
-  background: #fff;
-  font-family: "Baloo Da 2", sans-serif;
-  & > div {
-    padding: 15px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-  }
-
-  a {
-    text-decoration: none;
-  }
-
-  nav {
-    a {
-      display: inline-block;
-      margin-right: 22px;
-    }
-  }
-`
+import { ButtonGhostLink } from "../shared/buttonStyles"
 
 const LINKS = [
   {
@@ -50,38 +30,75 @@ const LINKS = [
   },
 ]
 
-// color: #111;
-// font-size: 28px;
-// text-transform: uppercase;
-// text-decoration: none;
-// font-weight: 600;
-
 const Header = ({ siteTitle }) => {
   const { colors } = useTheme()
-  const { red, blue, black, lightGray } = colors
+  const { red, blue, black, lightGray, white, textColor } = colors
+
+  const TopHeader = styled.header`
+    background: ${white};
+    .container,
+    .container-fluid {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      height: 80px;
+      @media (max-width: 767px) {
+        height: 60px;
+      }
+    }
+
+    a {
+      text-decoration: none;
+    }
+
+    nav {
+      a:not(:last-child) {
+        margin-right: 3.5rem;
+      }
+      a {
+        display: inline-block;
+      }
+      a:not(.btn) {
+        color: ${textColor};
+        font-weight: 600;
+        transition: color 220ms linear;
+        &:hover,
+        &.active {
+          color: ${blue};
+        }
+      }
+    }
+  `
+
   return (
     <TopHeader>
-      <div>
+      <div className="container-fluid">
         <Link
           css={css`
             color: ${black};
-            font-size: 20px;
-            text-transform: uppercase;
             text-decoration: none;
             font-weight: 600;
-            @media (min-width: 1024px) and (max-width: 1280px) {
-              font-size: 20px;
+            font-size: 2rem;
+            margin: 0;
+            @media (min-width: 1024px) {
+              font-size: 2.4rem;
             }
           `}
           to="/"
-          className="top-header__title"
+          className="h1 uppercase"
         >
           {siteTitle}
         </Link>
         <nav className="top-header__nav">
           {LINKS.map(({ id, title, urlPath }) => {
-            return (
-              <Link key={id} title={title} to={urlPath}>
+            const isContact = urlPath.match(/\/contact/gi)
+            const linkProps = { key: id, title, to: urlPath }
+            return isContact ? (
+              <ButtonGhostLink className="btn" {...linkProps}>
+                {title}
+              </ButtonGhostLink>
+            ) : (
+              <Link activeClassName="active" {...linkProps}>
                 {title}
               </Link>
             )
