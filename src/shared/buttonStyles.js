7a65590291b3e0117215196534069b034276ee86
Konstantin M <humbl3dev@gmail.com>
2020-03-28 12:57:09 -0700

    updates hover state for base button

--- src/shared/buttonStyles.js
+++ src/shared/buttonStyles.js
@@ -3,13 +3,12 @@ import { css } from "@emotion/core"
 import styled from "@emotion/styled"
 import tw from "twin.macro"
 
-import { siteTheme } from "./theme"
-
-const baseButton = tw`bg-blue-900 hover:bg-blue-900 text-white font-bold py-2 px-6 rounded leading-tight lg:text-xl`
+const baseButton = tw`bg-blue-900 hover:opacity-75 text-white font-bold py-2 px-6 rounded leading-tight lg:text-xl`
 const baseButtonOutline = tw`bg-transparent hover:bg-blue-900 text-blue-900 font-semibold hover:text-white py-1 px-4 border border-blue-900 hover:border-transparent rounded leading-tight lg:text-xl`
 
 const Button = styled.button`
   ${baseButton};
+  ${props => (props.fullWidth ? tw`w-full` : ``)};
   transition: 150ms linear;
 `
 const GhostButton = styled.button`
