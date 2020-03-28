fb86f89502d2bbc6ef9a8b7371c26a9168d00a52
Konstantin M <humbl3dev@gmail.com>
2020-03-28 12:27:55 -0700

    updates buttonStyles

--- src/shared/buttonStyles.js
+++ src/shared/buttonStyles.js
@@ -5,8 +5,8 @@ import tw from "twin.macro"
 
 import { siteTheme } from "./theme"
 
-const baseButton = tw`bg-blue-900 hover:bg-blue-900 text-white font-bold py-1 px-4 rounded`
-const baseButtonOutline = tw`bg-transparent hover:bg-blue-900 text-blue-900 font-semibold hover:text-white py-1 px-4 border border-blue-900 hover:border-transparent rounded`
+const baseButton = tw`bg-blue-900 hover:bg-blue-900 text-white font-bold py-2 px-6 rounded leading-tight lg:text-xl`
+const baseButtonOutline = tw`bg-transparent hover:bg-blue-900 text-blue-900 font-semibold hover:text-white py-1 px-4 border border-blue-900 hover:border-transparent rounded leading-tight lg:text-xl`
 
 const Button = styled.button`
   ${baseButton};
@@ -16,6 +16,10 @@ const GhostButton = styled.button`
   ${baseButtonOutline};
   transition: 150ms linear;
 `
+const ButtonLink = styled(Link)`
+  ${baseButton};
+  ${tw`no-underline`};
+`
 const GhostButtonLink = styled(Link)`
   ${baseButtonOutline};
   ${tw`no-underline`};
@@ -24,50 +28,4 @@ const GhostButtonLink = styled(Link)`
   }
 `
 
-export { Button, GhostButton, GhostButtonLink }
-
-const { colors } = siteTheme
-// const { blue, black, white, red } = colors
-// const buttonBase = css`
-//   appearance: none;
-//   color: ${white};
-//   background-color: ${black};
-//   border: 2px solid ${black};
-//   padding: 0.5rem 1rem;
-//   border-radius: 3px;
-//   font-weight: 600;
-// `
-// const ghostButtonBase = css`
-//   ${buttonBase};
-//   background: transparent;
-// `
-// const ghostButtonBlue = css`
-//   ${ghostButtonBase};
-//   color: ${blue};
-//   border-color: ${blue};
-// `
-// const ghostButtonRed = css`
-//   ${ghostButtonBase};
-//   color: ${red};
-//   border-color: ${red};
-// `
-// const Button = styled.button`
-//   ${buttonBase};
-//   background-color: ${props => (props.alt ? red : blue)};
-// `
-// const ButtonLink = styled(Link)`
-//   ${buttonBase};
-//   background-color: ${props => (props.alt ? red : blue)};
-//   text-decoration: none;
-// `
-// const ButtonGhost = styled`
-//     ${ghostButtonBase};
-//     ${props => (props.alt ? ghostButtonRed : ghostButtonBlue)}
-//   `
-// const ButtonGhostLink = styled(Link)`
-//   ${ghostButtonBase};
-//   ${props => (props.alt ? ghostButtonRed : ghostButtonBlue)};
-//   text-decoration: none;
-// `
-
-// export { Button, ButtonLink, ButtonGhost, ButtonGhostLink }
+export { Button, ButtonLink, GhostButton, GhostButtonLink }
