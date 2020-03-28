a3e103cd0391d32f7e0eaeff048eebf96839f94b
Konstantin M <humbl3dev@gmail.com>
2020-03-28 12:57:37 -0700

    contact form - WIP

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,46 +1,59 @@
 import React from "react"
+import { css } from "@emotion/core"
+import styled from "@emotion/styled"
+import tw from "twin.macro"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { Button } from "../shared/buttonStyles"
 
+const StyledLabel = styled.label`
+  ${tw`font-semibold inline-block text-dark mb-1 leading-tight cursor-pointer`}
+`
+const StyledInput = styled.input`
+  ${tw`leading-tight text-base py-2 px-2 border border-dark rounded outline-none focus:border-blue-900`}
+  ${props => (props.fullWidth ? tw`w-full` : ``)}
+`
+const StyledFieldSet = styled.fieldset`
+  ${tw`mb-5`}
+`
 const ContactUsPage = () => (
   <Layout>
     <SEO title="Contact Us" />
-    <h1>Contact Us</h1>
-    <p>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi, fuga
-      corporis? Voluptates fugit a odit, iste explicabo quod ab in neque veniam
-      dolor voluptatum aliquam inventore dolores repellendus dolorem. Officia
-      maxime, laboriosam delectus impedit dignissimos a quibusdam, dolorum
-      voluptates minus ea neque sit perferendis veritatis excepturi doloribus,
-      adipisci modi rem fugiat sint nihil deleniti. Repellendus quaerat
-      voluptatibus harum nisi reiciendis? Officia labore consectetur excepturi
-      officiis consequuntur expedita optio ab doloribus nam corrupti odit,
-      repellendus quidem ex distinctio amet? Reprehenderit, numquam amet,
-      similique illum doloremque commodi est soluta officia ipsum quod saepe
-      excepturi porro. Reprehenderit aliquam sit iste non rem voluptatem velit
-      facere labore! Nobis molestiae tenetur quam ipsa itaque iusto, fuga
-      assumenda omnis, voluptatibus laudantium blanditiis veniam? Pariatur eos
-      dolorem placeat, quis eaque dicta fugiat at velit beatae sint! At nobis
-      sapiente beatae aliquid tempora, hic quisquam sit officia provident, alias
-      fuga delectus perspiciatis porro adipisci distinctio quas quod soluta ex
-      modi nam repellendus ratione? Harum, dolorem eos? Tempore repudiandae
-      laboriosam neque provident pariatur. Ipsa perferendis ad placeat corrupti
-      nostrum nemo exercitationem facere tempore sed cum eos unde nulla maiores
-      ab ratione temporibus distinctio non labore eum magni, asperiores mollitia
-      repudiandae adipisci! Molestiae odit nulla expedita distinctio
-      exercitationem, natus qui sapiente. Porro qui eius vel ullam nulla,
-      mollitia ex ab facilis reprehenderit itaque recusandae fuga dolores.
-      Magnam rerum totam quos? Nisi consequatur minima, eligendi omnis eveniet
-      nam, iure qui, quos eaque hic quas excepturi aut voluptatum facilis labore
-      reiciendis ipsum assumenda molestias amet! Totam dolores, nulla commodi
-      laudantium quam aperiam dolorum quidem esse doloremque illo accusantium
-      optio tempora eaque et architecto suscipit molestiae ab earum dignissimos.
-      In labore, recusandae quod harum repudiandae neque minus, cupiditate
-      architecto dolorem tempore sequi magnam maiores. Tenetur minima voluptate,
-      molestiae ipsum dolorem iusto qui fugit similique delectus eveniet
-      sapiente, optio omnis odit sequi, soluta corporis.
-    </p>
+    <div
+      className="container"
+      css={css`
+        ${tw`max-w-sm mb-20 mt-10`}
+      `}
+    >
+      <h1 className="h1">Contact Us</h1>
+      <p>
+        Contact us for a free quote. We respond within 24 hours of your request.
+      </p>
+      <form>
+        <StyledFieldSet>
+          <StyledLabel htmlFor="name">Your name:</StyledLabel>
+          <StyledInput fullWidth id="name" type="text" />
+        </StyledFieldSet>
+        <StyledFieldSet
+          css={css`
+            ${tw`mt-3`}
+          `}
+        >
+          <StyledLabel htmlFor="email">Your Email:</StyledLabel>
+          <StyledInput fullWidth id="email" type="email" />
+        </StyledFieldSet>
+        <StyledFieldSet
+          css={css`
+            ${tw`mt-8`}
+          `}
+        >
+          <Button fullWidth type="submit">
+            Request Quote
+          </Button>
+        </StyledFieldSet>
+      </form>
+    </div>
   </Layout>
 )
 
