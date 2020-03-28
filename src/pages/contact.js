4debbf5faf12398fb09dceecad2c1913aee4273a
Konstantin M <humbl3dev@gmail.com>
2020-03-28 13:20:34 -0700

    contact form - WIP

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,8 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { css } from "@emotion/core"
 import styled from "@emotion/styled"
 import tw from "twin.macro"
+import { motion } from "framer-motion"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -10,51 +11,107 @@ import { Button } from "../shared/buttonStyles"
 const StyledLabel = styled.label`
   ${tw`font-semibold inline-block text-dark mb-1 leading-tight cursor-pointer`}
 `
-const StyledInput = styled.input`
+const StyledInput = styled(motion.input)`
   ${tw`leading-tight text-base py-2 px-2 border border-dark rounded outline-none focus:border-blue-900`}
   ${props => (props.fullWidth ? tw`w-full` : ``)}
 `
 const StyledFieldSet = styled.fieldset`
   ${tw`mb-5`}
 `
-const ContactUsPage = () => (
-  <Layout>
-    <SEO title="Contact Us" />
-    <div
-      className="container"
-      css={css`
-        ${tw`max-w-sm mb-20 mt-10`}
-      `}
-    >
-      <h1 className="h1">Contact Us</h1>
-      <p>
-        Contact us for a free quote. We respond within 24 hours of your request.
-      </p>
-      <form>
-        <StyledFieldSet>
-          <StyledLabel htmlFor="name">Your name:</StyledLabel>
-          <StyledInput fullWidth id="name" type="text" />
-        </StyledFieldSet>
-        <StyledFieldSet
-          css={css`
-            ${tw`mt-3`}
-          `}
-        >
-          <StyledLabel htmlFor="email">Your Email:</StyledLabel>
-          <StyledInput fullWidth id="email" type="email" />
-        </StyledFieldSet>
-        <StyledFieldSet
-          css={css`
-            ${tw`mt-8`}
-          `}
-        >
-          <Button fullWidth type="submit">
-            Request Quote
-          </Button>
-        </StyledFieldSet>
-      </form>
-    </div>
-  </Layout>
-)
+
+const useForm = () => {
+  const [inputs, setInputs] = useState(() => {
+    return {
+      name: "",
+      email: "",
+    }
+  })
+
+  const handleInputChange = (input, value) => {
+    setInputs({
+      ...inputs,
+      [input]: value,
+    })
+  }
+  useEffect(() => {
+    console.log("input updated")
+  }, [inputs])
+
+  return [inputs, handleInputChange]
+}
+
+const ContactUsPage = () => {
+  const [fieldInputs, handleInputChange] = useForm()
+
+  function handleFormSubmit(event) {
+    event.preventDefault()
+    const name = event.target.elements["name"].value
+    const email = event.target.elements["email"].value
+  }
+  return (
+    <Layout>
+      <SEO title="Contact Us" />
+      <div
+        className="container"
+        css={css`
+          ${tw`max-w-sm mb-20 mt-10`}
+        `}
+      >
+        <h1 className="h1">Contact Us</h1>
+        <p>
+          Contact us for a free quote. We respond within 24 hours of your
+          request.
+        </p>
+        <form onSubmit={handleFormSubmit}>
+          <StyledFieldSet>
+            <StyledLabel htmlFor="name">Your name:</StyledLabel>
+            <StyledInput
+              fullWidth
+              id="name"
+              name="name"
+              type="text"
+              onChange={e => handleInputChange(e.target.name, e.target.value)}
+              required
+            />
+          </StyledFieldSet>
+          <StyledFieldSet
+            css={css`
+              ${tw`mt-3`}
+            `}
+          >
+            <StyledLabel htmlFor="email">Your Email:</StyledLabel>
+            <StyledInput
+              fullWidth
+              id="email"
+              name="email"
+              type="email"
+              onChange={e => handleInputChange(e.target.name, e.target.value)}
+              required
+            />
+          </StyledFieldSet>
+          <StyledFieldSet
+            css={css`
+              ${tw`mt-8`}
+            `}
+          >
+            <Button
+              whileTap={{
+                scale: 0.85,
+              }}
+              initial={{ y: 20 }}
+              animate={{
+                y: 0,
+              }}
+              fullWidth
+              type="submit"
+            >
+              Request Quote
+            </Button>
+          </StyledFieldSet>
+        </form>
+      </div>
+    </Layout>
+  )
+}
 
 export default ContactUsPage
--- src/shared/buttonStyles.js
+++ src/shared/buttonStyles.js
@@ -2,16 +2,18 @@ import { Link } from "gatsby"
 import { css } from "@emotion/core"
 import styled from "@emotion/styled"
 import tw from "twin.macro"
+import { motion } from "framer-motion"
 
-const baseButton = tw`bg-blue-900 hover:opacity-75 text-white font-bold py-2 px-6 rounded leading-tight lg:text-xl`
+const baseButton = tw`bg-blue-900 border border-blue-900 hover:opacity-75 text-white font-bold py-2 px-6 rounded leading-tight lg:text-xl`
 const baseButtonOutline = tw`bg-transparent hover:bg-blue-900 text-blue-900 font-semibold hover:text-white py-1 px-4 border border-blue-900 hover:border-transparent rounded leading-tight lg:text-xl`
 
-const Button = styled.button`
+const Button = styled(motion.button)`
   ${baseButton};
   ${props => (props.fullWidth ? tw`w-full` : ``)};
+  ${props => (props.disabled ? tw`pointer-events-none opacity-50` : ``)};
   transition: 150ms linear;
 `
-const GhostButton = styled.button`
+const GhostButton = styled(motion.button)`
   ${baseButtonOutline};
   transition: 150ms linear;
 `
