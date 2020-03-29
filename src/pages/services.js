7579b41dc4145aea2600690c4f521927b679e35b
Konstantin M <humbl3dev@gmail.com>
2020-03-28 17:36:58 -0700

    services page placeholder content

--- src/pages/services.js
+++ src/pages/services.js
@@ -1,4 +1,6 @@
 import React from "react"
+import { css } from "@emotion/core"
+import tw from "twin.macro"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -6,41 +8,31 @@ import SEO from "../components/seo"
 const ServicesPage = () => (
   <Layout>
     <SEO title="Services" />
-    <h1>About Apkomatic</h1>
-    <p>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi, fuga
-      corporis? Voluptates fugit a odit, iste explicabo quod ab in neque veniam
-      dolor voluptatum aliquam inventore dolores repellendus dolorem. Officia
-      maxime, laboriosam delectus impedit dignissimos a quibusdam, dolorum
-      voluptates minus ea neque sit perferendis veritatis excepturi doloribus,
-      adipisci modi rem fugiat sint nihil deleniti. Repellendus quaerat
-      voluptatibus harum nisi reiciendis? Officia labore consectetur excepturi
-      officiis consequuntur expedita optio ab doloribus nam corrupti odit,
-      repellendus quidem ex distinctio amet? Reprehenderit, numquam amet,
-      similique illum doloremque commodi est soluta officia ipsum quod saepe
-      excepturi porro. Reprehenderit aliquam sit iste non rem voluptatem velit
-      facere labore! Nobis molestiae tenetur quam ipsa itaque iusto, fuga
-      assumenda omnis, voluptatibus laudantium blanditiis veniam? Pariatur eos
-      dolorem placeat, quis eaque dicta fugiat at velit beatae sint! At nobis
-      sapiente beatae aliquid tempora, hic quisquam sit officia provident, alias
-      fuga delectus perspiciatis porro adipisci distinctio quas quod soluta ex
-      modi nam repellendus ratione? Harum, dolorem eos? Tempore repudiandae
-      laboriosam neque provident pariatur. Ipsa perferendis ad placeat corrupti
-      nostrum nemo exercitationem facere tempore sed cum eos unde nulla maiores
-      ab ratione temporibus distinctio non labore eum magni, asperiores mollitia
-      repudiandae adipisci! Molestiae odit nulla expedita distinctio
-      exercitationem, natus qui sapiente. Porro qui eius vel ullam nulla,
-      mollitia ex ab facilis reprehenderit itaque recusandae fuga dolores.
-      Magnam rerum totam quos? Nisi consequatur minima, eligendi omnis eveniet
-      nam, iure qui, quos eaque hic quas excepturi aut voluptatum facilis labore
-      reiciendis ipsum assumenda molestias amet! Totam dolores, nulla commodi
-      laudantium quam aperiam dolorum quidem esse doloremque illo accusantium
-      optio tempora eaque et architecto suscipit molestiae ab earum dignissimos.
-      In labore, recusandae quod harum repudiandae neque minus, cupiditate
-      architecto dolorem tempore sequi magnam maiores. Tenetur minima voluptate,
-      molestiae ipsum dolorem iusto qui fugit similique delectus eveniet
-      sapiente, optio omnis odit sequi, soluta corporis.
-    </p>
+    <div
+      className="container-fluid"
+      css={css`
+         {
+          ${tw`my-16 max-w-lg`}
+        }
+      `}
+    >
+      <h1>Services</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet deleniti
+        dolorum quod, facilis, excepturi quas debitis exercitationem minima
+        magni aspernatur fugiat quisquam in? Velit quaerat pariatur,
+        voluptatibus eum nobis animi omnis vero quidem numquam, eius voluptates
+        laudantium. Optio ducimus vitae facere harum, deleniti earum doloribus
+        doloremque in, voluptatibus temporibus velit?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+        aspernatur dolore nostrum est ex quas delectus, veritatis iure
+        cupiditate repellendus ipsa atque sint ullam, nobis enim consequuntur
+        architecto eum odit officia nemo deserunt! A quia nobis corporis
+        suscipit provident quam.
+      </p>
+    </div>
   </Layout>
 )
 
